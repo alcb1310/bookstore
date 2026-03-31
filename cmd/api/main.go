186e0f1fdd142812bcb9104f25c76a164c4b6588
Andres Court <andres@andrescourt.com>
2026-03-30 20:28:31 -0500

    feat(route): read the port from the environment file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/alcb1310/bookstore/internal/router"
+import (
+	"log/slog"
+	"os"
+	"strconv"
+
+	"github.com/alcb1310/bookstore/internal/router"
+
+	_ "github.com/joho/godotenv/autoload"
+)
 
 func main() {
-	var port uint16 = 42069
+	port64, err := strconv.ParseUint(os.Getenv("PORT"), 10, 16)
+	if err != nil {
+		slog.Error("Error parsing port", "error", err)
+		panic(err)
+	}
+	port := uint16(port64)
 
 	s := router.New(port)
 	if err := s.Router(); err != nil {

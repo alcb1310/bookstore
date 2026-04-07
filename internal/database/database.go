003d352951058e7ef2b3e7f4be4d3a3e77779397
Andres Court <andres@andrescourt.com>
2026-04-06 20:47:51 -0500

    handle database ping errors

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,10 +1,15 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/alcb1310/bookstore/internal/interfaces"
+	"github.com/jackc/pgx/v5/pgconn"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
@@ -33,5 +38,39 @@ func New() (Service, error) {
 }
 
 func (s *service) HealthCheck() error {
-	return s.DB.Ping()
+	if err := s.DB.PingContext(context.Background()); err != nil {
+		if e, ok := errors.AsType[*pgconn.PgError](err); ok {
+			switch e.Code {
+			case "3D000":
+				return &interfaces.APIError{
+					Code:          http.StatusGatewayTimeout,
+					Msg:           "Database is not available",
+					OriginalError: e,
+				}
+			case "28000":
+				return &interfaces.APIError{
+					Code:          http.StatusBadGateway,
+					Msg:           "Database is not available",
+					OriginalError: e,
+				}
+			default:
+				return &interfaces.APIError{
+					Code:          http.StatusInternalServerError,
+					Msg:           "Database is not available",
+					OriginalError: e,
+				}
+			}
+		}
+		if _, ok := errors.AsType[*pgconn.ConnectError](err); ok {
+			return &interfaces.APIError{
+				Code:          http.StatusServiceUnavailable,
+				Msg:           "Database is not available",
+				OriginalError: err,
+			}
+		}
+
+		return err
+	}
+
+	return nil
 }
